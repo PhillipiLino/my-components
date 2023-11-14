710733f52ca90e120d9c6b13fbe0a242ad720545
PhillipiLino <phillipi.lino@gmail.com>
2023-11-14 20:53:04 -0300

    Put web to root

--- lib/my_components.dart
+++
@@ -1,8 +0,0 @@
-export 'package:heroicons/heroicons.dart';
-
-export 'src/components/buttons/main_button.dart' show MainButtonSize;
-export 'src/components/buttons/my_button.dart';
-export 'src/extensions/color_extensions.dart';
-export 'src/extensions/context_extensions.dart';
-export 'src/my_text_styles.dart';
-export 'src/theme_manager/theme_manager.dart';
