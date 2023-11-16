e9ec635722707af94a5b55b9841e1614a23258fb
PhillipiLino <phillipi.lino@gmail.com>
2023-11-16 09:30:53 -0300

    Add new properties to chart

--- lib/src/components/my_bar_chart.dart
+++ lib/src/components/my_bar_chart.dart
@@ -12,14 +12,22 @@ class MyChartData {
 
 class MyBarChart extends StatefulWidget {
   final List<MyChartData> items;
+  final String? selectedXValue;
   final double? goal;
   final bool inverted;
   final Widget Function(int index)? yLabelBuilder;
+  final Widget Function(int index)? xLabelBuilder;
+  final Function(int index, MyChartData data)? onTapBar;
+  final ScrollController? scrollController;
 
   const MyBarChart({
     required this.items,
+    this.selectedXValue,
     this.goal,
     this.yLabelBuilder,
+    this.xLabelBuilder,
+    this.onTapBar,
+    this.scrollController,
     this.inverted = false,
     super.key,
   });
@@ -34,10 +42,16 @@ class _MyBarChartState extends State<MyBarChart> {
   @override
   Widget build(BuildContext context) {
     final list = widget.items;
+    if (widget.selectedXValue != null) {
+      selectedIndex = list.indexWhere(
+        (element) => element.xValue == widget.selectedXValue,
+      );
+    }
+
     return LayoutBuilder(
       builder: (context, constraints) {
         final height = constraints.maxHeight;
-        const textHeight = 25.0;
+        const textHeight = 20.0;
         late final size = height - (textHeight * 2);
         final goalLine = size - (size * (widget.goal ?? 0)) + textHeight;
 
@@ -57,6 +71,7 @@ class _MyBarChartState extends State<MyBarChart> {
               ListView.separated(
                 itemCount: list.length,
                 scrollDirection: Axis.horizontal,
+                controller: widget.scrollController,
                 separatorBuilder: (_, __) => SizedBox(width: spacings.xsmall),
                 itemBuilder: (context, index) {
                   final marginTop = (size * list[index].yValue);
@@ -66,11 +81,13 @@ class _MyBarChartState extends State<MyBarChart> {
                   final textOpacity = isSelected ? 1.0 : 0.4;
                   final textStyle = MyTextStyle.regular();
 
-                  return SizedBox(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Opacity(
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      GestureDetector(
+                        onTap: () => widget.onTapBar?.call(index, list[index]),
+                        child: Opacity(
                           opacity: textOpacity,
                           child: widget.yLabelBuilder?.call(index) ??
                               Text(
@@ -78,46 +95,50 @@ class _MyBarChartState extends State<MyBarChart> {
                                 style: textStyle,
                               ),
                         ),
-                        InkWell(
-                          hoverColor: Colors.transparent,
-                          highlightColor: Colors.transparent,
-                          splashColor: Colors.transparent,
-                          onTapCancel: () => setState(() => selectedIndex = -1),
-                          onTapUp: (_) => setState(() => selectedIndex = -1),
-                          onTapDown: (_) {
-                            setState(() => selectedIndex = index);
-                          },
-                          onHover: (hover) {
-                            setState(() => selectedIndex = hover ? index : -1);
-                          },
-                          child: AnimatedContainer(
-                            width: 30,
-                            duration: const Duration(milliseconds: 100),
-                            padding: EdgeInsets.only(top: marginTop),
-                            decoration: BoxDecoration(
-                              boxShadow: [
-                                if (selectedIndex == index)
-                                  BoxShadow(
-                                    color: mainColor.withOpacity(0.5),
-                                    spreadRadius: 0.1,
-                                    blurRadius: 8,
-                                    offset: const Offset(0, 3),
-                                  ),
-                              ],
-                              borderRadius: BorderRadius.vertical(
-                                top: Radius.circular(borderRadius.small),
-                                bottom: Radius.circular(borderRadius.small),
+                      ),
+                      InkWell(
+                        hoverColor: Colors.transparent,
+                        highlightColor: Colors.transparent,
+                        splashColor: Colors.transparent,
+                        onTap: () => widget.onTapBar?.call(index, list[index]),
+                        onTapCancel: () => setState(() => selectedIndex = -1),
+                        onTapUp: (_) => setState(() => selectedIndex = -1),
+                        onTapDown: (_) => setState(() => selectedIndex = index),
+                        onHover: (hover) {
+                          setState(() => selectedIndex = hover ? index : -1);
+                        },
+                        child: Column(
+                          children: [
+                            AnimatedContainer(
+                              width: 30,
+                              duration: const Duration(milliseconds: 100),
+                              padding: EdgeInsets.only(top: marginTop),
+                              decoration: BoxDecoration(
+                                color: isSelected ? mainColor : secondaryColor,
+                                boxShadow: [
+                                  if (selectedIndex == index)
+                                    BoxShadow(
+                                      color: mainColor.withOpacity(0.5),
+                                      spreadRadius: 0.1,
+                                      blurRadius: 8,
+                                      offset: const Offset(0, 3),
+                                    ),
+                                ],
+                                borderRadius: BorderRadius.vertical(
+                                  top: Radius.circular(borderRadius.small),
+                                  bottom: Radius.circular(borderRadius.small),
+                                ),
                               ),
-                              color: isSelected ? mainColor : secondaryColor,
                             ),
-                          ),
+                            Opacity(
+                              opacity: textOpacity,
+                              child: widget.xLabelBuilder?.call(index) ??
+                                  Text(list[index].xValue, style: textStyle),
+                            ),
+                          ],
                         ),
-                        Opacity(
-                          opacity: textOpacity,
-                          child: Text(list[index].xValue, style: textStyle),
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   );
                 },
               ),
@@ -129,8 +150,9 @@ class _MyBarChartState extends State<MyBarChart> {
                   child: Container(
                     height: borderWidths.small,
                     decoration: BoxDecoration(
-                      color:
-                          colors.feedbackColors.success.dark.withOpacity(0.5),
+                      color: colors.primary.withOpacity(
+                        0.5,
+                      ),
                     ),
                   ),
                 ),
